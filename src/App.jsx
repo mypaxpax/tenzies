71e5e7f2391e6b6c6bf8f5d52dc35fd10adcf009
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-02-09 20:59:56 +0100

    render die components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Die from "./components/Die";
 
 function App() {
   return (
     <main>
-      <div className="container"> Hello! 👋</div>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={7} />
+        <Die value={8} />
+        <Die value={9} />
+        <Die value={10} />
+      </div>
     </main>
   );
 }
