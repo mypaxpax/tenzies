c3c4ee6c600572bb1ea3e4c9861ea99619ab10c7
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-02-10 15:14:29 +0100

    dice numbers set in state from array

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,21 @@ import { useState } from "react";
 import Die from "./components/Die";
 
 function App() {
-  function allNewDice() {
-    let diceArray = [];
-    for (let i = 0; i < 10; i++) {
-      diceArray.push(Math.ceil(Math.random() * 6));
-      console.log("Added number");
-    }
-    return diceArray;
-  }
-  console.log(diceArray);
-  allNewDice();
+  const [diceNum, setDiceNum] = useState(allNewDice());
 
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceElements = diceNum.map((dice) => <Die value={dice} />);
+  console.log(diceElements);
   return (
     <main>
-      <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={10} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 }
