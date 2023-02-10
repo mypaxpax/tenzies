f3a6f80e1624a6de206f39d11bccc809a541ed62
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-02-10 17:06:30 +0100

    Dice value check for win condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const heldDice = dice.every((die) => die.isHeld);
+    const firstVal = dice[0].value;
+    const AllVals = dice.every((die) => die.value === firstVal);
+    if (heldDice && AllVals) {
+      console.log("Won!");
+    }
+  }, [dice]);
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateDice());
     }
     return newDice;
   }
 
+  function generateDice() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateDice();
+      })
+    );
   }
 
   function holdDice(id) {
@@ -40,6 +58,11 @@ function App() {
   ));
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
       <button onClick={rollDice}>Reroll!</button>
     </main>
